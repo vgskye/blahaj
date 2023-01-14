66981d24b7a7ef35cf47fe2a4382b9ad25f4328b
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-01-14 23:08:57 +0800

    trivial stuff

--- src/commands/uwurandom.ts
+++ src/commands/uwurandom.ts
@@ -6,13 +6,11 @@ export const uwurandomCommand: Command = async (i) => {
   const length = i.options.getInteger('length', true);
 
   let uwurandom = Uwurandom.new();
-  let result = "";
+  let result = '';
 
   for (let i = 0; i < length; i++) {
     result += uwurandom.generate();
   }
 
-  await i.reply({
-    content: result,
-  });
+  await i.reply(result);
 };
